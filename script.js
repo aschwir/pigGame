7ed39447f038741ed8d10958291e2b1e8a258bce
andrew schwirzinski <aschwirzinski@gmail.com>
2022-07-06 11:29:32 -0400

    set dice to its inital state at the start of the game

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 'use strict';
 
-//selecting score elements
+//selecting elements
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
+diceEl.classList.add('hidden');
