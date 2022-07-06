23459ce34b33794f2e4d8aa5b29ae0de7b64c02d
andrew schwirzinski <aschwirzinski@gmail.com>
2022-07-06 13:37:29 -0400

    roll dice functionality and display dice

--- script.js
+++ script.js
@@ -4,7 +4,22 @@
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHole = document.querySelector('.btn--hold');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+//rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  //generate random dice roll (1-6)
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  //display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  //check for rolled 1 : if true switch to next player
+});
