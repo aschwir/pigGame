fceaed605c57126f252ddf9725733ffc41c1ed92
andrew schwirzinski <aschwirzinski@gmail.com>
2022-07-07 12:21:35 -0400

    add current score to total score and display

--- script.js
+++ script.js
@@ -21,6 +21,14 @@ const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer == 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
 //rolling dice functionality
 btnRoll.addEventListener('click', function () {
   //generate random dice roll (1-6)
@@ -37,10 +45,19 @@ btnRoll.addEventListener('click', function () {
       currentScore;
   } else {
     //switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer == 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchPlayer();
   }
+});
+
+btnHole.addEventListener('click', function () {
+  //add current score to active player
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  //check score is >= 100
+  //finish the game
+
+  //switch to the next player
+  switchPlayer();
 });
