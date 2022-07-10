74569b7118a5666f2513111d2aafc042eeeef43c
andrew schwirzinski <aschwirzinski@gmail.com>
2022-07-09 23:21:36 -0400

    new game functionality refactored

--- script.js
+++ script.js
@@ -13,14 +13,27 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = scores[0];
+  score1El.textContent = scores[1];
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -78,22 +91,4 @@ btnHold.addEventListener('click', function () {
   }
 });
 
-btnNew.addEventListener('click', function () {
-  playing = true;
-  //set player one as active player
-  player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--winner');
-  player0El.classList.add('player--active');
-  player1El.classList.remove('player--active');
-  //clear current score and total scores of both players
-  scores[0] = 0;
-  scores[1] = 0;
-  console.log(scores);
-  current0El.textContent = 0;
-  current1El.textContent = 0;
-  score0El.textContent = scores[0];
-  score1El.textContent = scores[1];
-
-  currentScore = 0;
-  //clear current score and total scores of both players
-});
+btnNew.addEventListener('click', init);
